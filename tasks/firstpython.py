07ee0ae23f11254d92e8bc1838539b6fbbe31945
K.G <kg@example.com>
2023-01-05 16:50:15 -0600

    Python hello-worlds a CSV filepath

--- tasks/firstpython.py
+++ tasks/firstpython.py
@@ -1,8 +1,11 @@
 from cumulusci.tasks.salesforce import BaseSalesforceApiTask
 import os
+import csv
 
 this_python_codebase_path = os.path.realpath(os.path.dirname(__file__))
 up_one_folder = os.path.abspath(os.path.join(this_python_codebase_path, '..'))
+sfdmu_play_folder = os.path.abspath(os.path.join(up_one_folder, 'sfdmu-play'))
+sfdmu_play_account_csv_filepath = os.path.abspath(os.path.join(sfdmu_play_folder, 'Account.csv'))
 
 class YayForPython(BaseSalesforceApiTask):
     # _run_task function declaration makes this code runnable with a "cci task run" command
@@ -10,4 +13,4 @@ class YayForPython(BaseSalesforceApiTask):
         # "self.sf" & "self.org_config" represent org-login state that the "cci task run" context knows.
         # "self.options" is a dict-like data structure of details you put into your "cumulusci.yml" file.
         # get_org_schema() returns a Python object called "cumulusci.salesforce_api.org_schema.Schema".
-        self.logger.info('Hello world')
+        self.logger.info('Hello world' + ' ' + sfdmu_play_account_csv_filepath)
